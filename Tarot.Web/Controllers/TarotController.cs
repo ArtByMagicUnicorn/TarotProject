cb2d7eb6c07e1dcac3465d05d81c09f34ee94e3a
Martina Halldin <82marhal@gafe.molndal.se>
2025-11-20 11:38:31 +0100

    Card details

--- Tarot.Web/Controllers/TarotController.cs
+++ Tarot.Web/Controllers/TarotController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Tarot.Data;
 using Microsoft.EntityFrameworkCore;
+using Tarot.Web.Models;
 using System.Linq;
 
 
@@ -20,42 +21,112 @@ namespace Tarot.Web.Controllers
 
             return View(allCards);
         }
-        // Lägg till denna nya metod i TarotController.cs
 
         public async Task<IActionResult> DrawToday()
         {
+            // 1. Hämta ett slumpmässigt kort
             var randomCard = await _context.TarotCards
-
-
-                .Include(c => c.Meanings)       // <-- 1. Hämta alla betydelser för kortet
-                    .ThenInclude(m => m.MeaningCategory) // <-- 2. Hämta kategorin FÖR VARJE betydelse
-
-                .OrderBy(c => EF.Functions.Random())
-                .FirstOrDefaultAsync();
+                                           .Include(c => c.Meanings)
+                                               .ThenInclude(m => m.MeaningCategory)
+                                           .OrderBy(c => EF.Functions.Random())
+                                           .FirstOrDefaultAsync();
 
             if (randomCard == null)
             {
                 return NotFound("Kunde inte hitta några kort i leken!");
             }
 
-            return View(randomCard);
+            // 2. NU: Singla slant om det ska vara omvänt!
+            var random = new Random();
+            bool isReversed = random.Next(2) == 0; // 50% chans
+
+            // 3. Slå in paketet i vår ViewModel
+            var drawnCard = new DrawnCard
+            {
+                Card = randomCard,
+                IsReversed = isReversed
+            };
+
+            // 4. Skicka paketet (DrawnCard) till vyn
+            return View(drawnCard);
         }
 
-        // Lägg till denna nya metod i TarotController.cs
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Hämta kortet med matchande ID + dess betydelser
+            var card = await _context.TarotCards
+                .Include(c => c.Meanings)
+                    .ThenInclude(m => m.MeaningCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
 
         public async Task<IActionResult> ThreeCardSpread()
         {
-            // Precis som förut, men vi använder ".Take(3)"
-            // för att hämta de 3 första korten från den slumpade listan.
-            var threeCards = await _context.TarotCards
-                                           .Include(c => c.Meanings)
-                                               .ThenInclude(m => m.MeaningCategory)
-                                           .OrderBy(c => EF.Functions.Random())
-                                           .Take(3)
-                                           .ToListAsync(); // Notera: ToListAsync() istället för FirstOrDefault!
+            // 1. Hämta 3 slumpmässiga kort från databasen
+            var cardsFromDb = await _context.TarotCards
+                                            .Include(c => c.Meanings)
+                                                .ThenInclude(m => m.MeaningCategory)
+                                            .OrderBy(c => EF.Functions.Random())
+                                            .Take(3)
+                                            .ToListAsync();
 
-            // Vi skickar en LISTA med 3 kort till vyn
-            return View(threeCards);
+            // 2. Skapa vår lista med "Omslagspapper" (ViewModels)
+            var drawnCards = new List<DrawnCard>();
+            var random = new Random();
+
+            foreach (var card in cardsFromDb)
+            {
+                // Singla slant! (0 eller 1)
+                // Om det blir 0 så är IsReversed = true (omvänt)
+                bool upsideDown = random.Next(2) == 0;
+
+                drawnCards.Add(new DrawnCard
+                {
+                    Card = card,
+                    IsReversed = upsideDown
+                });
+            }
+
+            // 3. Skicka listan med DRAWNCARDS (inte TarotCards) till vyn
+            return View(drawnCards);
+        }
+        public async Task<IActionResult> CelticCross()
+        {
+            // 1. Hämta 10 slumpmässiga kort
+            var cardsFromDb = await _context.TarotCards
+                                            .Include(c => c.Meanings)
+                                                .ThenInclude(m => m.MeaningCategory)
+                                            .OrderBy(c => EF.Functions.Random())
+                                            .Take(10) // <--- TIO KORT!
+                                            .ToListAsync();
+
+            // 2. Skapa ViewModels (hantera omvända kort)
+            var drawnCards = new List<DrawnCard>();
+            var random = new Random();
+
+            foreach (var card in cardsFromDb)
+            {
+                bool isReversed = random.Next(2) == 0;
+                drawnCards.Add(new DrawnCard
+                {
+                    Card = card,
+                    IsReversed = isReversed
+                });
+            }
+
+            return View(drawnCards);
         }
     }
 }
---
+++ Tarot.Web/Models/DrawnCard.cs
@@ -0,0 +1,11 @@
+﻿namespace Tarot.Web.Models 
+{ 
+
+    using Tarot.Data;
+
+    public class DrawnCard
+    {
+        public TarotCard Card { get; set; }
+        public bool IsReversed { get; set; }
+    }
+}
